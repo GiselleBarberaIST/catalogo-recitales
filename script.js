2038c78f8d96c8e616f9d72d4a3349a558e4943a
Giselle Barbera <giselle.barbera.93@gmail.com>
2025-11-13 19:36:15 -0300

    Se agregan cambios a script.js para adaptar a lectura desde Airtable, y se agregan imágenes al proyecto. También se modifican JSONs

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const token = "patEqV2sPUQuOgfbP.d999c807bf015abc0f70feadf7a082e0de0080a36360ae7
 const baseID = "appdhZ2FVCDUuj4YT";
 const baseUrl = `https://api.airtable.com/v0/${baseID}`;
 
-async function obtenerDatos(tableName) {
-  const url = `${baseUrl}/${tableName}`;
+async function obtenerDatos(tabla) {
+  const url = `${baseUrl}/${tabla}`;
   const response = await fetch(url, {
     headers: { Authorization: `Bearer ${token}`}
   });
@@ -12,7 +12,7 @@ async function obtenerDatos(tableName) {
   return data.records.map(r => r.fields);
 }
 
-(async () => {
+/*(async () => {
   const conciertos = await obtenerDatos("Conciertos");
   const favoritos = await obtenerDatos("Favoritos");
   const pantallaConciertos = await obtenerDatos("PantallaConciertos");
@@ -20,7 +20,7 @@ async function obtenerDatos(tableName) {
   console.log("Conciertos: ", conciertos);
   console.log("Favoritos: ", favoritos);
   console.log("Pantalla de Conciertos: ", pantallaConciertos);
-})();
+})();*/
 
 // ---MENÚ HAMBURGUESA---
 const menuHamburguesa = document.getElementById('menuHamburguesa');
@@ -45,9 +45,10 @@ async function cargarCartelera() {
     const slide = document.createElement("div");
     slide.className = "poster";
     if (i === 0) slide.classList.add("active");
+    
     slide.innerHTML = `
-      <a href = "${c.DetalleURL}">
-        <img src = "${c.Imagen[0].url}" alt = "${c.Nombre}">
+      <a href = "${c.DetalleURL || '#'}" target = "_blank">
+        <img src = "${c.Imagen[0]?.url || ''}" alt = "${c.NombreConcierto || ''}">
       </a>
     `;
     contenedorCartelera.appendChild(slide);
@@ -82,13 +83,20 @@ async function cargarCartelera() {
 
 cargarCartelera();
 
-// ---BUSCADOR---
-document.addEventListener("DOMContentLoaded", async() => {
+// ---RENDERS---
+document.addEventListener("DOMContentLoaded", async () => {
   const conciertos = await obtenerDatos("Conciertos");
-  console.log("Conciertos cargados desde Airtable: ", conciertos);
+  const favoritos = await obtenerDatos("Favoritos");
+  const tickteras = await obtenerDatos("Ticketeras");
 
-  fetch('buscador.html')
-    .then(response => response.text())
+  console.log("Conciertos: ", conciertos);
+  console.log("Favoritos: ", favoritos);
+  console.log("Ticketeras: ", ticketeras);
+})
+
+// ---BUSCADOR---
+fetch('buscador.html')
+    .then(res => res.text())
     .then(html => {
       document.getElementById('cont-buscador').innerHTML = html;
 
@@ -101,27 +109,21 @@ document.addEventListener("DOMContentLoaded", async() => {
       const modalBuscador = document.getElementById('modalBuscador');
       const btnAbrirBuscador = document.getElementById('abrirBuscador');
       const cerrarModalBuscador = modalBuscador.querySelector('#cerrar-modal-buscador');
-
-      desdeInput?.setAttribute("min", fechaHoy);
-      hastaInput?.setAttribute("min", fechaHoy);
-
-      btnAbrirBuscador.addEventListener('click', (e) => {
-        e.preventDefault();
-        modalBuscador.style.display = 'flex';
-      });
-
-      cerrarModalBuscador.addEventListener('click', () => {
-        modalBuscador.style.display = 'none';
-      });
-
-      window.addEventListener('click', (e) => {
-        if (e.target === modalBuscador) modalBuscador.style.display = 'none';
-      });
-
       const modalResultados = document.getElementById('modalResultados');
       const cerrarResultadosX = modalResultados.querySelector('#cerrar-modal-resultados');
       const cerrarResultadosBtn = document.getElementById('cerrar-modal-boton');
 
+      desdeInput?.setAttribute("min", fechaHoy);
+      hastaInput?.setAttribute("min", fechaHoy);
+
+      btnAbrirBuscador.addEventListener('click', e => {
+        e.preventDefault();
+        modalBuscador.style.display = 'flex';
+      });
+
+      cerrarModalBuscador.addEventListener('click', () => modalBuscador.style.display = 'none');
+      window.addEventListener('click', e => { if (e.target === modalBuscador) modalBuscador.style.display = 'none';});
+      
       cerrarResultadosX?.addEventListener('click', () => {
         modalResultados.style.display = 'none';
       });
@@ -135,8 +137,8 @@ document.addEventListener("DOMContentLoaded", async() => {
       form.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        const artista = artistaInput.value.trim().toLowerCase;
-        const provincia = provinciaSelect.value.trim().toLowerCase;
+        const artista = artistaInput.value.trim().toLowerCase();
+        const provincia = provinciaSelect.value.trim().toLowerCase();
         const fechaDesde = desdeInput.value;
         const fechaHasta = hastaInput.value;
 
@@ -151,8 +153,8 @@ document.addEventListener("DOMContentLoaded", async() => {
         const resultados = conciertos.filter(c => {
           const fechaEvento = parseFecha(c.Fecha);
           return (
-            (!artista || (c.Artista && c.Artista.toLowerCase().includes(artista))) &&
-            (provincia === "todas" || (c.Provincia && c.Provincia.toLowerCase() === provincia)) &&
+            (!artista || (c.Artista?.toLowerCase().includes(artista))) &&
+            (provincia === "todas" || (c.Provincia?.toLowerCase() === provincia)) &&
             (!desde || fechaEvento >= desde) &&
             (!hasta || fechaEvento <= hasta)
           );
@@ -171,13 +173,40 @@ document.addEventListener("DOMContentLoaded", async() => {
               <h3>${c.Artista || "Artista sin nombre"}</h3>
               <p>${c.Provincia || "Provincia no especificada"} - ${formatoDDMMYYYY(c.Fecha) || "Fecha no especificada"}</p>
               <a href="${c.URL || '#'}" target="_blank">Comprar entradas</a>
-              <button>Agregar a Favoritos</button>
+              <button class = "btn-favorito">Agregar a Favoritos</button>
             `;
             resultadosContainer.appendChild(div);
+
+            const botonFav = div.querySelector('btn-favorito');
+            botonFav.addEventListener('click', async () => {
+              const nombreUsuario = localStorage.getItem("nombreUsuario");
+
+              if (!nombreUsuario) {
+                alert("Para agregar a Favoritos necesitás registrarte o iniciar sesión.");
+                return;
+              }
+
+              const datos = {
+                ID_Concierto: c.ID_Concierto || "Sin ID",
+                NombreUsuario: nombreUsuario,
+                ImagenConcierto: [
+                  { url: c.Imagen[0]?.url || "https:/via.placeholder.com/150" }
+                ],
+                Ticketera: c.URL || "",
+                FechaGuardado: new Date().toISOString().split('T')[0]
+              };
+
+              try {
+                const res = await agregarAFavoritos(datos);
+                console.log("Favorito agregado: ", res);
+                alert(`${c.NombreConcierto} se agregó a tus Favoritos`);
+              } catch (error) {
+                console.error("Error al agregar Favorito: ", error);
+              }
+            })
           });
         }
-
-        modalResultados.style.display = "flex";
+        modalResultados.style.display = "flex",
         modalBuscador.style.display = "none";
       });
   })
@@ -195,50 +224,45 @@ document.addEventListener("DOMContentLoaded", async() => {
   }
 
 // --FAVORITOS
-const favoritos = await obtenerDatos("Favoritos");
+async function agregarAFavoritos(datosFavorito) {
+  try {
+    const response = await fetch('/api/crearRegistro', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({ datos: datosFavorito })
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error('Error al agregar Favorito: ', error);
+  }
+}
+
 const contenedorFav = document.querySelector(".favoritos-usuario");
 if (contenedorFav) {
   favoritos.forEach(f => {
     const card = document.createElement("div");
-    const url = document.createElement("a");
-    url.href = f.URL;
-    url.target = "_blank";
-
-    const img = document.createElement("img");
-    img.src = f.Imagen[0].url;
-    img.alt = f.Nombre;
-
-    url.appendChild(img);
-    const nombre = document.createElement("p");
-    nombre.textContent = f.Nombre;
-
-    card.appendChild(url);
-    card.appendChild(nombre);
+    card.innerHTML = `
+    <a href = "${f.URL}" target = "_blank">
+    <img src = "${f.Imagen[0]?.url}" alt = "${f.NombreConcierto || ''}">
+    </a>
+    <p>${f.NombreConcierto || "Favorito sin nombre"}</p>
+    `;
     contenedorFav.appendChild(card);
   });
 }
 
 // --TICKETERAS
-  const ticketeras = await obtenerDatos("Ticketeras");
-  const contenedorTick = document.querySelector(".ticketeras-principales");
-  if (contenedorTick) {
-    ticketeras.forEach(t => {
-      const card = document.createElement("div");
-      const url = document.createElement("a");
-      url.href = t.URL;
-      url.target = "_blank";
-
-      const img = document.createElement("img");
-      img.src = t.Imagen[0].url;
-      img.alt = t.Nombre;
-
-      url.appendChild(img);
-      const nombre = document.createElement("p");
-      nombre.textContent = t.Nombre;
-
-      card.appendChild(url);
-      card.appendChild(nombre);
-      contenedorTick.appendChild(card);
-    });
-  }
-});
+const contenedorTick = document.querySelector(".ticketeras-principales");
+if (contenedorTick) {
+  ticketeras.forEach(t => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+    <a href = "${t.URL}" target = "_blank">
+      <img src = "${f.Imagen[0]?.url}" alt = "${f.NombreConcierto || ''}">
+    </a>
+    <p>${f.NombreConcierto || ''}</p>
+    `;
+    contenedorTick.appendChild(card);
+  });
+}
